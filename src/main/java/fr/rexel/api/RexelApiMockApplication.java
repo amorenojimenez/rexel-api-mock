72996b79a0532a097418845b89c6f4e5365ae29c
Alberto MORENO <a.moreno.jimenez@gmail.com>
2025-10-15 14:39:55 +0200

    Initial commit

---
+++ src/main/java/fr/rexel/api/RexelApiMockApplication.java
@@ -0,0 +1,13 @@
+package fr.rexel.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RexelApiMockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RexelApiMockApplication.class, args);
+    }
+
+}
